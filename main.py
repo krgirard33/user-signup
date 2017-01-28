0234defa76d038f6d7f756087a775a274b1abe2a
krgirard33 <ken.girard@gmail.com>
2017-01-28 16:14:04 -0600

    REGEX, Welcome code, and not working

--- main.py
+++ main.py
@@ -19,31 +19,30 @@ import cgi
 
 
 
-page_header = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Signup Form</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>
-        <a href="/">Signup Form</a>
-    </h1>
-"""
+      
 
-# html boilerplate for the bottom of every page
-page_footer = """
-    </body>
-</html>
-"""
+def build_page(textarea_content):
+    page_header = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Signup Form</title>
+            <style type="text/css"> 
+                .error {color: red;}
+            </style>
+        </head>
+    <body>
+        <h1>
+                <a href="/">Signup Form</a>
+        </h1>  """
 
-# html signin form 
-form_signup = """
+    # html boilerplate for the bottom of every page
+    page_footer = """
+        </body>
+    </html> """
+
+    # html signin form 
+    form_signup = """
         <form method="post">
             <table>
                 <tr>
@@ -77,13 +76,72 @@ form_signup = """
             </table>
             <input type="submit">
         </form> """
+    return page_header + form_signup + page_footer 
 
+
+USERNAME_REGEX = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USERNAME_REGEX.match(username)
+
+
+PASSWORD_REGEX = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASSWORD_REGEX.match(password)
+
+
+EMAIL_REGEX = re.compile(r"[^@\s]+@[^@\s]+\.[^@\s.]+$")
+def valid_email(email):
+    return not email or EMAIL_REGEX.match(email)
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page("")
+        self.response.write(content)
+
+    
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('werify')
+        email = self.request.get('email')
+
+        # this stores the username and email so we can return it if there is an error
+        params = dict(username = username,
+                      email = email)
+       
+       if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.response.write(content, **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(Basehandler):
+    def get(self):
+        username = self.request.get('username'):
+        if valid_username(username):
+            self.response.write(content, username=username)
+        else:
+            self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/signup', Signup),
+    ('/welcome', Welcome),
 ], debug=True)
